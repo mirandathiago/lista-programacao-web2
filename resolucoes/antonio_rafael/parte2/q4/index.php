555e9f72f4a0c5333eb19ed0b5c475628af87698
antoniorafaelsouzaneves <202415210056@ifba.edu.br>
2025-06-15 12:57:12 -0300

    Questão 4 - Parte 2 Respondida

---
+++ resolucoes/antonio_rafael/parte2/q4/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Raízes da Equação do Segundo Grau</title>
+</head>
+<body>
+    <form action = "resposta.php" method = "post"> 
+        <label> Valor de A: 
+            <input type="number" placeholder = "Digite o valor de A" name = "valorA">
+       </label>
+
+        <label> Valor de B: 
+            <input type="number" placeholder = "Digite o valor de B" name = "valorB">
+       </label>
+
+        <label> Valor de C: 
+            <input type="number" placeholder = "Digite o valor de C" name = "valorC">
+       </label>
+
+       <button> Calcular </button>
+    </form>
+</body>
+</html>
