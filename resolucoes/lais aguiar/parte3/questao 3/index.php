a5bae5f8c8c10d3453575a5e96166263950c2f62
LAIsAgUiArrr <202415210047@ifba.edu.br>
2025-06-14 21:37:23 -0300

    tudo atualizado

--- resolucoes/lais aguiar/parte3/questao 3/index.php
+++ resolucoes/lais aguiar/parte3/questao 3/index.php
@@ -1,21 +1,34 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
     <title>Contagem de Números Pares</title>
 </head>
 <body>
     <h2>Informe o intervalo de números:</h2>
-    <form method="post">
-        <label>Início do intervalo:</label>
-        <input type="number" name="inicio" required><br><br>
-
-        <label>Fim do intervalo:</label>
-        <input type="number" name="fim" required><br><br>
-
-        <button type="submit">Calcular</button>
+    <form method="post" action="questao3.php">
+        <label >Início do intervalo:</label>
+        <input type="number" name="inicio"><br><br>
+        
+        <label >Fim do intervalo:</label>
+        <input type="number" name="fim" ><br><br>
+        
+        <button >Calcular</button>
     </form>
 
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $inicio = intval($
+        $inicio = $_POST["inicio"];
+        $fim = $_POST["fim"];
+        $quantidade_pares = 0;
+
+        for ($i = $inicio; $i <= $fim; $i++) {
+            if ($i % 2 == 0) {
+                $quantidade_pares++;
+            }
+        }
+
+        echo "<h3>Quantidade de números pares: $quantidade_pares</h3>";
+    }
+    ?>
+</body>
+</html>
