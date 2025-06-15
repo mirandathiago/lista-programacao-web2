223aec1c73a40415eadcf381ff6c284d13008b01
antoniorafaelsouzaneves <202415210056@ifba.edu.br>
2025-06-15 15:31:38 -0300

    Questão 1 - Parte 3 - Respondida

---
+++ resolucoes/antonio_rafael/parte3/q1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fatorial</title>
+</head>
+<body>
+    <form action = "resposta.php" method = "post"> 
+        <label> Digite um número inteiro: 
+            <input type = "number" placeholder = "Digite um número inteiro" name = "num">
+        </label>
+
+        <button> Calcular fatorial </button>
+    </form>
+</body>
+</html>
